b08261bc466257dcf6719f557f50523ad1471e51
giladt2789 <giladdavidt@gmail.com>
2022-10-12 15:24:22 +0300

    testing

--- Terraform_EKS/outputs.tf
+++ Terraform_EKS/outputs.tf
@@ -1,10 +1,10 @@
-output "cluster_name" {
-  value = aws_eks_cluster.EKS.name
-}
+# output "cluster_name" {
+#   value = aws_eks_cluster.EKS.name
+# }
 
-output "cluster_endpoint" {
-  value = aws_eks_cluster.EKS.endpoint
-}
+# output "cluster_endpoint" {
+#   value = aws_eks_cluster.EKS.endpoint
+# }
 
 output "cluster_ca_certificate" {
   value = aws_eks_cluster.EKS.certificate_authority[0].data
